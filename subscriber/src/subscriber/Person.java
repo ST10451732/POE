9ba9a6f839f4f4943078686cbbbf5caf80e8f27b
Kholofelo <164382086+ST10451732@users.noreply.github.com>
2024-04-12 13:16:08 +0200

    Add files via upload

---
+++ subscriber/src/subscriber/Person.java
@@ -0,0 +1,18 @@
+package subscriber;
+
+	
+public class Person {
+	private String subFullname;
+	private String subEmail;
+	private String subPhonenumber;
+	
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+		this.subFullname = subFullname;
+		this.subEmail = subEmail;
+		this.subPhonenumber = subPhonenumber;
+		
+		
+	}
+}
